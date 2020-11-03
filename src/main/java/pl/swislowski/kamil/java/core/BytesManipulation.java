f2d988555c533c219169635e9aaa6bf1c32f0ced
Kamil Świsłowski <kamil.swislowski@gmail.com>
2020-11-03 20:39:56 +0100

    Add javadocs and small refactor.

--- src/main/java/pl/swislowski/kamil/java/core/BytesManipulation.java
+++ src/main/java/pl/swislowski/kamil/java/core/BytesManipulation.java
@@ -1,49 +1,50 @@
 package pl.swislowski.kamil.java.core;
 
-import java.util.Arrays;
 import java.util.logging.Logger;
 
 public class BytesManipulation {
     private static final Logger LOGGER = Logger.getLogger(BytesManipulation.class.getName());
 
+    /**
+     * Dla danego ciągu bajtów <code>bytes</code> wyszukuje <code>wantedBytes<code/> i zamienia na
+     * <code>swapBytes</code>.
+     * @param bytes ciąg bajtów do modyfikacji
+     * @param wantedBytes wyszukiwany ciąg bajtów
+     * @param swapBytes ciąg bajtów na który zamieniamy wyszukiwany ciąg bajtów
+     * @return zwraca zmodyfikowany ciąg bajtów
+     */
     public byte[] replaceBytes(byte[] bytes, byte[] wantedBytes, byte[] swapBytes) {
 
         if (bytes != null && wantedBytes != null && swapBytes != null
                 && wantedBytes.length > 0 && swapBytes.length > 0) {
 
             int startIndex = findStartIndex(bytes, wantedBytes);
+            if (startIndex != -1) {
 
-            int length = wantedBytes.length;
-            if (swapBytes.length > wantedBytes.length) {
-                length = swapBytes.length;
-            }
-
-            for (int i = startIndex; i < startIndex + length; i++) {
-//            System.out.println("bytes[" + i + "] = " + bytes[i]);
-//                System.out.println("bytes[" + i + "] = " + (char) bytes[i]);
-//            System.out.println(bytes[i]);
-//            bytes[i] = (byte)'x';
-
-                int index = i - startIndex;
-                if (index < swapBytes.length) {
-                    bytes[i] = swapBytes[index];
+                int length = wantedBytes.length;
+                if (swapBytes.length > wantedBytes.length) {
+                    length = swapBytes.length;
                 }
 
-                //01234567891011
-                //bbbbwwwwwbbb
-                //....sssss...
+                for (int i = startIndex; i < startIndex + length; i++) {
+
+                    int index = i - startIndex;
+                    if (index < swapBytes.length) {
+                        bytes[i] = swapBytes[index];
+                    }
+                }
             }
-//TODO : zamienić na swapBytes zamiast xxxxxxxx.
-            System.out.println(Arrays.toString(bytes));
-            System.out.println(new String(bytes));
-
-
         }
         return bytes;
     }
 
+    /**
+     * Wyszukuje indeks startowy ciągu bajtów <code>wantedBytes</code> w ciągu bajtów <code>bytes</code>.
+     * @param bytes przeszukiwany ciąg bajtów
+     * @param wantedBytes wyszukiwany ciąg bajtów
+     * @return Zwraca indeks startowy wyszukiwanego ciągu bajtów.
+     */
     int findStartIndex(byte[] bytes, byte[] wantedBytes) {
-//        LOGGER.info("replace method invocation external class ##############");
         for (int i = 0; i < bytes.length - wantedBytes.length + 1; ++i) {
             boolean found = true;
             for (int j = 0; j < wantedBytes.length; ++j) {
